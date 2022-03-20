1f0b9292eb079af46b65494be009f996c1c03db9
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2022-03-20 21:21:01 +0100

    Add files for 1

---
+++ 1/1_2.sh
@@ -0,0 +1,10 @@
+
+npm version
+npm install -g m
+m latest
+mongod --version
+brew tap mongodb/brew
+brew install mongosh
+mongosh --version
+brew install mongodb-database-tools
+mongoimport
---
+++ 1/1_3.sh
@@ -0,0 +1,7 @@
+npm version
+sudo npm install -g m
+m latest
+mongod --version
+docs.mongodb.com/mongodb-shell/install/
+
+sudo apt-get install mongodb-database-tools
